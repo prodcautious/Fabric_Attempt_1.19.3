b2dec7d5d60af9503f23f0aff0f4340382ef6894
Nick Phillips <thisisforspam628@gmail.com>
2023-02-20 13:49:47 -0500

    adding files

---
+++ src/main/java/net/cautious/slugcore/world/gen/SlugCoreTreeGeneration.java
@@ -0,0 +1,14 @@
+package net.cautious.slugcore.world.gen;
+
+import net.cautious.slugcore.world.SlugCorePlacedFeatures;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.world.biome.BiomeKeys;
+import net.minecraft.world.gen.GenerationStep;
+
+public class SlugCoreTreeGeneration {
+    public static void generateTrees() {
+        BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.PLAINS, BiomeKeys.FOREST),
+                GenerationStep.Feature.VEGETAL_DECORATION, SlugCorePlacedFeatures.BLOOD_PLACED_KEY);
+    }
+}
---
+++ src/main/java/net/cautious/slugcore/world/gen/SlugCoreWorldGeneration.java
@@ -0,0 +1,8 @@
+package net.cautious.slugcore.world.gen;
+
+public class SlugCoreWorldGeneration {
+
+    public static void generateSlugCoreWorldGen() {
+        SlugCoreTreeGeneration.generateTrees();
+    }
+}
